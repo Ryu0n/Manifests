c52d35a9606eb1cf1d28a246dc2b43ebcbe3495c
Ryu0n <hsung951027@gmail.com>
2024-03-26 11:56:05 +0900

    u: airflow docker 수정

--- docker/airflow/setup-airflow.sh
+++ docker/airflow/setup-airflow.sh
@@ -1,6 +1,11 @@
 #! /bin/bash
 
-curl -LfO 'https://airflow.apache.org/docs/apache-airflow/2.8.0/docker-compose.yaml'
+if [ -f docker-compose.yaml ]; then
+    echo "docker-compose.yaml is already exists."
+else
+    curl -LfO 'https://airflow.apache.org/docs/apache-airflow/2.8.0/docker-compose.yaml'
+fi
+
 
 mkdir -p ./dags ./logs ./plugins ./config
 echo -e "AIRFLOW_UID=$(id -u)" > .env
