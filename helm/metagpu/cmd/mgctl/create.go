e50446716a78750f949ac9ccbfe1d3942b86e7a3
Ryu0n <hsung951027@gmail.com>
2025-02-23 11:17:21 +0900

    d: 미사용 Helm Chart 제거

--- helm/metagpu/cmd/mgctl/create.go
+++
@@ -1,3 +0,0 @@
-package main
-
-// TODO: create logic for container and device visibility level tokens generations
--- helm/metagpu/hack/scripts/test2.py
+++
@@ -1,4 +0,0 @@
-import tensorflow as tf
-tf.get_logger().setLevel('INFO')
-gpus = tf.config.list_physical_devices('GPU')
-print(gpus)
